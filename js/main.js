9bd56203429ab855385fee4218afe130b13e8d14
Pascal O.Y <xinpascal@gmail.com>
2013-09-06 17:41:59 +0800

    2013-09-06 Update the site.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($){
+	// For highlight blocks
+	$(function() {
+        if (window.hljs) {
+            $('pre > code').each(function(i, e) { hljs.highlightBlock(e); });
+        }
+
+        $('article').on('click', '[href="#"], [href=""]', function (e) {
+            e.preventDefault();
+        });
+    });
+
+})(jQuery);
